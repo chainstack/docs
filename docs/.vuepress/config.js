8aa5048721a396326de5c99fd506e4ff55fb4179
Eugene Aseev <14873170+easeev@users.noreply.github.com>
2019-05-23 15:43:14 +0800

    Added GA tracking and bumped VuePress version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,6 +57,9 @@ module.exports = {
         ]
     },
     plugins: {
+        '@vuepress/google-analytics': {
+            'ga': 'UA-118674508-7'
+        },
         'clean-urls': {
             normalSuffix: ''            
         },
