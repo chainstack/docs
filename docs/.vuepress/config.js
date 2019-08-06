2a5f1eb7c4b853d5bafca75744c0c74b2b32a56a
Ake Gaviar <evkonst@gmail.com>
2019-08-06 15:52:12 +0300

    Move making geth to tools
    Move the instructions to geth interaction tools instead
    of keeping it as a separate topic

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -80,7 +80,6 @@ module.exports = {
                            '/operations/quorum/default-addresses',
                            '/operations/quorum/key-management',
                            '/operations/quorum/tools',
-                           '/operations/quorum/building-quorum-geth-from-source-on-ubuntu',
 
                           ]
                      },
