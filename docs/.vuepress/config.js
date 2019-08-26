f2bf252418363ae7509fdfc108717e972be4375b
Phap Duong <d2phap@gmail.com>
2019-08-26 14:18:50 +0800

    updated header bar UI
    #108

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,11 @@ module.exports = {
     description: "Chainstack documentation",
     head: [
         ['link', { rel: 'icon', href: './favicon.ico' }]
-      ],
+    ],
     themeConfig: {
+        logo: '/img/docs-logo.svg',
         nav: [
-            { text: 'Support', link: 'https://support.chainstack.com/' },
-            { text: 'Log in', link: 'https://console.chainstack.com' },
+            { text: 'Console', link: 'https://console.chainstack.com' },
             { text: 'Try free', link: 'https://chainstack.com/pricing/' }
         ],
         sidebar: [
