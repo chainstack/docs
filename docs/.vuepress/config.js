2441f0c29688f793ed85cba84c63f85a10c1258c
Ake Gaviar <10195782+akegaviar@users.noreply.github.com>
2022-06-22 18:04:12 +0800

    Update docs/.vuepress/config.js
    Co-authored-by: Eugene Aseev <14873170+easeev@users.noreply.github.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -83,7 +83,7 @@ module.exports = {
                             '/operations/ethereum/types',
                             '/operations/ethereum/modes',
                             '/operations/ethereum/mev-api',
-                            '/operations/ethereum/debug-and-trace-api',
+                            '/operations/ethereum/debug-and-trace-apis',
                             '/operations/ethereum/clients',
                             '/operations/ethereum/tools',
                         ]
